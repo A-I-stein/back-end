a64cf9f80d9f4fb9503298a186595e7e300bb623
Arthur <arthurfm@live.com>
2019-10-07 15:10:21 -0300

    Add Filtro CORS

---
+++ JAVA/src/main/java/com/tcc/filter/FiltroCORSGeral.java
@@ -0,0 +1,41 @@
+package com.tcc.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Arthur
+ */
+public class FiltroCORSGeral implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Methods", 
+                "POST, GET, PUT, OPTIONS, DELETE");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.addHeader("Access-Control-Max-Age", "86400");
+        response.setContentType("text/html;charset=UTF-8");
+        req.setCharacterEncoding("utf-8");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
