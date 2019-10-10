aa82ea0430832969b4f55de4d81868402afcf929
Gabriel Cruz <gabriel.gomesgalo@gmail.com>
2019-10-10 19:10:00 -0300

    Criação da Classe UserAccessService
    Responsável pela conexão da classe User com o Banco de Dados

--- JAVA/src/main/java/com/aistein/model/service/UserAccessService.java
+++ JAVA/src/main/java/com/aistein/model/service/UserAccessService.java
@@ -39,11 +39,97 @@ public class UserAccessService {
         NOME = "Nome";
         EMAIL = "Email";
         SENHA = "Senha";
-        DATA_CADASTRO = "DataCadastro";
-        DATA_NASCIMENTO = "DataNascimento";
-        IDT_TIPO = "IdentificadorTipo";
+        DATA_CADASTRO = "Data_Cadastro";
+        DATA_NASCIMENTO = "Data_Nascimento";
+        IDT_TIPO = "Identificador_Tipo";
         FOTO = "Foto";
         GENERO = "Genero";
     }
+
+     /**
+     * Pesquisa no bd se o Username já está cadastrado.
+     * @param Username
+     * @return se o username já está sendo utilizado por outro Usuario.
+     */
+    public static boolean isUsernameUsed(String username) {
+        
+        ArrayList<User> usuarios = query("SELECT * FROM usuario WHERE Username="
+                                       + username);
+        
+        if (usuarios == null){
+            System.out.println("Nome está disponivel" + 
+                    username);
+            return false;
+        }
+        
+        return true;
+    }
+
+     /**
+     * Pesquisa no bd usando o Username do Usuário.
+     * @param Username
+     * @return um objeto Usuario que corresponde ao username recebido.
+     */
+    public static User getUserFromUsername(String username) {
+        
+        ArrayList<User> usuarios = query("SELECT * FROM usuario WHERE Username="
+                                       + username);
+        
+        if (isUsernameUsed(username)==false){
+            System.out.println("Nenhum usuário encontrado com esse username" + 
+                    username);
+            return null;
+        }
+        return usuarios.get(0);
+    }
+    
+    
+    /**
+     * Pesquisa no bd todas os alugueis.
+     * @return todos os objetos User em um ArrayList.
+     */
+    public static ArrayList<User> getAll() {
+        return query("SELECT * FROM usuario");
+    }
+    
+    /**
+     * Deleta um usuario do bd usando o username do mesmo.
+     * @param username do usuario a ser removido.
+     * @return true se a operação for bem sucedida e false se não for.
+     */
+    public static boolean delete(int id) {
+        String sql = "DELETE FROM usuario WHERE username = " + id;
+        return query(sql);
+    }
+        
+    /**
+     * Insere um Usuario no bd
+     * @param usuario
+     * @return true se não houver problemas na operação.
+     */
+    public static boolean insert(User usuario){
+
+        return SQL.insert(aluguel);
+    }
+        
+    /**
+     * Atualiza o usuario no bd correspondente ao objeto Usuario recebido
+     * @param user
+     * @return true se não houver problemas na operação.
+     */
+    public static boolean update(User usuario){
+         
+        String sql = "UPDATE usuario SET (Nome, Identificador_Tipo, Foto, Gener"
+                   + "o, Data_Nascimento, Senha) = (" +usuario.getNome()+ ", " 
+                   +  usuario.getIdtTipo() + ", " + usuario.getFoto() + ", " 
+                   + usuario.getGenero() + ", " + usuario.getDataNascimento", " 
+                   + usuario.getSenha + ") WHERE username = " 
+                   + usuario.getUsername;
+
+        return query(sql) == null;
+    }
+
+    
+    
     
 }
--- JAVA/src/main/java/com/aistein/model/table/User.java
+++ JAVA/src/main/java/com/aistein/model/table/User.java
@@ -11,6 +11,7 @@ import java.util.Date;
  *
  * @author Arthur
  */
+@Tabela("usuario")
 public class User {
     @Id
     private String username;
