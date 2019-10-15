6c1cc13a26f0d24a87b2a2a2ce418e49ae630448
Gabriel <gabriel.gomesgalo@gmail.com>
2019-10-15 11:07:33 -0300

    Atualização da classe Resposta
    A classe Resposta foi atualizada para corresponder às atuais necessidades do sistema

--- JAVA/src/main/java/com/aistein/model/table/Resposta.java
+++ JAVA/src/main/java/com/aistein/model/table/Resposta.java
@@ -8,34 +8,110 @@
 package com.aistein.model.table;
 
 /**
+ * Classe Modelo para uma Resposta.
+ * Tem como responsabilidade única servir como modelo para uma resposta.
  *
- * @author Arthur
+ * @author Arthur Marcolino, Gabriel Cruz
+ * @version 1.2
  */
-public class Resposta {
-    private String respostaAberta, respostaFechada;
 
+public class Resposta{
+    private int codResposta, codPergunta, codQuestionario;
+    private String descResposta;
+
+    /**
+     *  Construtor padrão.
+     */
     public Resposta() {
     }
+    
+    /**
+     * Construtor com dados iniciais.
+     *
+     * @param codResposta (Código) único da resposta.
+     * @param descResposta (Descrição) da resposta, ou seja, a resposta.
+     * @param codQuestionario (Código) do questionário ao pertence a pergunta 
+     *                         que a resposta pertence.
+     * @param codPergunta (Código) do pergunta ao qual a resposta 
+     *                         pertence.
+     */
+    public Resposta(int codResposta, String descResposta, int codPergunta, int codQuestionario) {
 
-    public Resposta(String respostaAberta, String respostaFechada) {
-        this.respostaAberta = respostaAberta;
-        this.respostaFechada = respostaFechada;
-    }
-
-    public String getRespostaAberta() {
-        return respostaAberta;
-    }
-
-    public void setRespostaAberta(String respostaAberta) {
-        this.respostaAberta = respostaAberta;
-    }
-
-    public String getRespostaFechada() {
-        return respostaFechada;
-    }
-
-    public void setRespostaFechada(String respostaFechada) {
-        this.respostaFechada = respostaFechada;
+        this.codResposta = codResposta;
+        this.descResposta = descResposta;
+        this.codPergunta = codPergunta;
+        this.codQuestionario = codQuestionario;
     }
     
+    /**
+     * Método que retorna o valor do atributo codResposta.
+     * 
+     * @return codResposta da resposta
+     */
+    public int getCodResposta() {
+        return codResposta;
+    }
+
+    /**
+     * Método que define o valor do atributo codResposta
+     * 
+     * @param codResposta
+     */
+    public void setCodResposta(int codResposta) {
+        this.codResposta = codResposta;
+    }
+
+    /**
+     * Método que retorna o valor do atributo descResposta.
+     * 
+     * @return descResposta da matéria
+     */
+    public String getDescResposta() {
+        return descResposta;
+    }
+
+    /**
+     * Método que define o valor do atributo descResposta
+     * 
+     * @param descResposta
+     */
+    public void setDescResposta(String descResposta) {
+        this.descResposta = descResposta;
+    }  
+    
+    /**
+     * Método que retorna o valor do atributo codQuestionario.
+     * 
+     * @return codQuestionario a qual a resposta pertence
+     */
+    public int getCodQuestionario() {
+        return codQuestionario;
+    }
+
+    /**
+     * Método que define o valor do atributo codQuestionario
+     * 
+     * @param codQuestionario
+     */
+    public void setCodQuestionario(int codQuestionario) {
+        this.codQuestionario = codQuestionario;
+    }
+    
+    /**
+     * Método que retorna o valor do atributo codPergunta.
+     * 
+     * @return codPergunta a qual a resposta pertence
+     */
+    public int getCodPergunta() {
+        return codPergunta;
+    }
+
+    /**
+     * Método que define o valor do atributo codPergunta
+     * 
+     * @param codPergunta
+     */
+    public void setCodPergunta(int codPergunta) {
+        this.codPergunta = codPergunta;
+    }
 }
