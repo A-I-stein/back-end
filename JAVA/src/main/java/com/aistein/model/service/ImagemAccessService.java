726de3c4c6961cf1b8a7a1db9a74326360c96456
Gabriel Cruz <gabriel.gomesgalo@gmail.com>
2019-10-16 13:38:16 -0300

    Criação da classe ImagemAccessService
    Responsável pela conexão da classe Imagem com o Banco de Dados

---
+++ JAVA/src/main/java/com/aistein/model/service/ImagemAccessService.java
@@ -0,0 +1,136 @@
+
+/*
+ * Projeto de Integracao - A.I.Stein
+ * CEFET-MG 
+ * INF-3A 2019
+ * Arthut Marcolino, Gabriel Cruz, Heitor Santos, Italo Nascimento
+ */
+
+package com.aistein.model.service;
+
+import com.aistein.model.table.Imagem;
+import com.aistein.util.SQL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Classe Conexão com a Imagem e o BD.
+ * Tem como responsabilidade única servir como conexão entre a classe modelo 
+ * Imagem com o Banco de Dados.
+ *
+ * @author Gabriel Cruz
+ * @version 1.0
+ */
+
+public class ImagemAccessService {
+    
+    //Objetos de manipulação interna.
+    private static ResultSet result;
+    
+    //Constantes que representam os nomes das colunas no DB SQL.
+    private static final String CODIGO_IMAGEM;
+    private static final String CAMINHO_IMAGEM;
+    
+    //Inicialização das constantes
+    static{
+        CODIGO_IMAGEM = "Codigo_Imagem";
+        CAMINHO_IMAGEM = "Nome_Imagem";
+    }
+
+    /**
+     * Pesquisa no bd usando a query recebida.
+     * @param query query a ser executada
+     * @return os objetos Imagem encontrados utilizando a query recebida.
+     */
+    public static ArrayList<Imagem> get(String query){
+        
+        ArrayList<Imagem> imagens = new ArrayList<>();
+        
+        try{
+            result = SQL.query(query);
+            
+            if(result.next()) {
+                do{
+                    Imagem imagem = new Imagem(
+                            result.getInt(CODIGO_IMAGEM), 
+                            result.getString(CAMINHO_IMAGEM));
+                                        
+                    imagens.add(imagem);
+                    
+                }while(result.next());
+            }else{
+                System.out.println("Nada encontrado com a query fornecida.");
+            }
+        }catch(SQLException ex) {
+            System.out.println("Ocorreu o Erro:" + ex);
+            return null;
+        }
+        return imagens;
+    }
+    
+    /**
+     * Pesquisa no bd usando o Codigo da Imagem.
+     * @param codImagem
+     * @return um objeto Imagem que corresponde ao codImagem recebido.
+     */
+    public static Content getImagemFromCodImagem(String codImagem) {
+        
+        ArrayList<Imagem> imagens = get("SELECT * FROM imagem WHERE"
+                                     + CODIGO_IMAGEM + "=" + codImagem);
+        
+        if (imagens==null){
+            System.out.println("Nenhum gênero encontrado com esse Código" + 
+                    codImagem);
+            return null;
+        }
+        return imagens.get(0);
+    }
+    
+    /**
+     * Pesquisa no bd todas as imagens.
+     * @return todos os objetos Imagem em um ArrayList.
+     */
+    public static ArrayList<Imagem> getAll() {
+        return get("SELECT * FROM imagem");
+    }
+    
+    /**
+     * Deleta um imagem do bd usando o codigo da mesma.
+     * @param codImagem do imagem a ser removido.
+     * @return true se a operação for bem sucedida e false se não for.
+     */
+    public static boolean delete(String codImagem) {
+        String stm = "DELETE FROM imagem WHERE " + CODIGO_IMAGEM + " = "
+                   + codImagem;
+        return SQL.query(stm) == null;
+    }
+        
+    /**
+     * Insere um Imagem no bd
+     * @param imagem
+     * @return true se não houver problemas na operação.
+     */
+    public static boolean insert(Imagem imagem){
+
+        String stm = "INSERT INTO imagem (" + CODIGO_IMAGEM + ", " 
+                   + CAMINHO_IMAGEM + ") VALUES ('" + imagem.getCodImagem() 
+                   + "', '" + imagem.getCaminhoImagem() + "');" ;
+
+        return SQL.query(stm) == null;
+    }
+        
+    /**
+     * Atualiza a imagem no bd correspondente ao objeto Imagem recebido
+     * @param imagem
+     * @return true se não houver problemas na operação.
+     */
+    public static boolean update(Imagem imagem){
+         
+        String stm = "UPDATE imagem SET (" + CAMINHO_IMAGEM + ") = ("
+                   + imagem.getCaminhoImagem()+")" + " WHERE " + CODIGO_IMAGEM
+                   + " = " + imagem.getCodImagem();
+
+        return SQL.query(stm) == null;
+    }
+}
