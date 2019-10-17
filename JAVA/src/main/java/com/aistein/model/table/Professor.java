c278e96c8b81c35a273488dd7657e16730bade8a
Gabriel <gabriel.gomesgalo@gmail.com>
2019-10-17 10:57:25 -0300

    Atualização das classes que herdam de User
    Para adequar melhor as necessidades do projeto

--- JAVA/src/main/java/com/aistein/model/table/Professor.java
+++ JAVA/src/main/java/com/aistein/model/table/Professor.java
@@ -7,46 +7,101 @@
 
 package com.aistein.model.table;
 
+import java.util.Date;
+
 /**
+ * Classe Modelo para um Professor.
+ * Tem como responsabilidade única servir como modelo para um professor.
  *
- * @author Arthur
+ * @author Arthur Marcolino, Gabriel Cruz
+ * @version 1.2
  */
-public class Professor {
-    private String criaPergunta, escola;
-    private boolean removePergunta;
 
+public class Professor extends User{
+    
+    private String username, escola;
+    private int materia;
+    
+    /**
+     *  Construtor padrão.
+     */
     public Professor() {
+        super();
     }
-
-    public Professor(String criaPergunta, String escola, boolean removePergunta) {
-        this.criaPergunta = criaPergunta;
+    
+    /**
+     * Construtor com dados iniciais.
+     * 
+     * @param username único do Usuário.
+     * @param nome do Usuário.
+     * @param email do Usuário.
+     * @param senha criada pelo Usuário.
+     * @param dataNascimento (Data de Nascimento) do Usuário.
+     * @param foto fornecida pelo Usuário.
+     * @param genero do Usuário.
+     * @param escola que o Professor Frequenta
+     * @param materia na qual o Professor é um especialista
+     */
+    public Professor(String username, String nome, String email, String senha,  
+                 Date dataNascimento, int foto, int genero, String escola, 
+                 int materia) {
+        super(nome, email, senha, username, dataNascimento, 'P', foto, genero);
         this.escola = escola;
-        this.removePergunta = removePergunta;
+        this.username = username;
+        this.materia = materia;
+    }
+    
+  /**
+     * Método que retorna o valor do atributo username.
+     * 
+     * @return username do usuario
+     */
+    public String getUsername() {
+        return username;
     }
 
-    public String getCriaPergunta() {
-        return criaPergunta;
+    /**
+     * Método que define o valor do atributo username
+     * 
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setCriaPergunta(String criaPergunta) {
-        this.criaPergunta = criaPergunta;
-    }
-
+    /**
+     * Método que retorna o valor do atributo escola.
+     * 
+     * @return escola do administrador
+     */
     public String getEscola() {
         return escola;
     }
 
+    /**
+     * Método que define o valor do atributo escola
+     * 
+     * @param escola
+     */
     public void setEscola(String escola) {
         this.escola = escola;
     }
 
-    public boolean isRemovePergunta() {
-        return removePergunta;
+    /**
+     * Método que retorna o valor do atributo materia.
+     * 
+     * @return materia do administrador
+     */
+    public int getMateria() {
+        return materia;
     }
 
-    public void setRemovePergunta(boolean removePergunta) {
-        this.removePergunta = removePergunta;
+    /**
+     * Método que define o valor do atributo materia
+     * 
+     * @param materia
+     */
+    public void setMateria(int materia) {
+        this.materia = materia;
     }
-    
-    
 }
