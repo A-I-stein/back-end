729b68296ea4dd0e0b89ac160167a9e1f074df1e
Arthur <arthurfm@live.com>
2019-10-21 12:36:40 -0300

    Adicionado Base Services

---
+++ JAVA/src/main/java/com/aistein/model/service/RespostaAcessService.java
@@ -0,0 +1,15 @@
+/*
+ * Projeto de Integracao - A.I.Stein
+ * CEFET-MG 
+ * INF-3A 2019
+ * Arthut Marcolino, Gabriel Cruz, Heitor Santos, Italo Nascimento
+ */
+package com.aistein.model.service;
+
+/**
+ *
+ * @author Arthur
+ */
+public class RespostaAcessService {
+    
+}
