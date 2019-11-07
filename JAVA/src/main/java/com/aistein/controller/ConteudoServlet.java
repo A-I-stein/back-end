91a56aaaab878e52209d586c3e092e8e1c1d6232
Marcola <arthurfm@live.com>
2019-11-07 08:04:34 -0300

    Resolvido problema do GlassFish

--- JAVA/src/main/java/com/aistein/controller/ConteudoServlet.java
+++ JAVA/src/main/java/com/aistein/controller/ConteudoServlet.java
@@ -1,102 +1,102 @@
-/*
- * Projeto de Integracao - A.I.Stein
- * CEFET-MG
- * INF-3A 2019
- * Arthut Marcolino, Gabriel Cruz, Heitor Santos, Italo Nascimento
- */
-package com.aistein.controller;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author Arthur
- */
-public class ConteudoServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-              Conteudo frontConteudo = JSON.parse(request.getParameter("tipo"),
-                           Patrimonio.class);
-               String resposta = "";
-               String req = request.getParameter("req");
-               String tipo = request.getParameter("tipo");
-               Boolean res = false;
-
-
-               switch (req) {
-                   case "all":
-                     ArrayList<Conteudo> conteudo = ConteudoAcessService.getAll();
-                     System.out.println(conteudo.toString());
-                       resposta = conteudo.toString();
-                   break;
-                   case "especifico":
-                     Conteudo conteudo = ConteudoAcessService.getConteudoFromNomConteudo(frontConteudo);
-                     System.out.println(conteudo.toString());
-                       resposta = conteudo.toString();
-                   break;
-                   case "codigo":
-                     Conteudo conteudo = ConteudoAcessService.getConteudoCodConteudo(frontConteudo);
-                     System.out.println(conteudo.toString());
-                       resposta = conteudo.toString();
-                   break;
-                   default:
-               }
-               try (PrintWriter out = response.getWriter()) {
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
-}
+///*
+// * Projeto de Integracao - A.I.Stein
+// * CEFET-MG
+// * INF-3A 2019
+// * Arthut Marcolino, Gabriel Cruz, Heitor Santos, Italo Nascimento
+// */
+//package com.aistein.controller;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+///**
+// *
+// * @author Arthur
+// */
+//public class ConteudoServlet extends HttpServlet {
+//
+//    /**
+//     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+//     * methods.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//              Conteudo frontConteudo = JSON.parse(request.getParameter("tipo"),
+//                           Patrimonio.class);
+//               String resposta = "";
+//               String req = request.getParameter("req");
+//               String tipo = request.getParameter("tipo");
+//               Boolean res = false;
+//
+//
+//               switch (req) {
+//                   case "all":
+//                     ArrayList<Conteudo> conteudo = ConteudoAcessService.getAll();
+//                     System.out.println(conteudo.toString());
+//                       resposta = conteudo.toString();
+//                   break;
+//                   case "especifico":
+//                     Conteudo conteudo = ConteudoAcessService.getConteudoFromNomConteudo(frontConteudo);
+//                     System.out.println(conteudo.toString());
+//                       resposta = conteudo.toString();
+//                   break;
+//                   case "codigo":
+//                     Conteudo conteudo = ConteudoAcessService.getConteudoCodConteudo(frontConteudo);
+//                     System.out.println(conteudo.toString());
+//                       resposta = conteudo.toString();
+//                   break;
+//                   default:
+//               }
+//               try (PrintWriter out = response.getWriter()) {
+//        }
+//    }
+//
+//    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+//    /**
+//     * Handles the HTTP <code>GET</code> method.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    /**
+//     * Handles the HTTP <code>POST</code> method.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    /**
+//     * Returns a short description of the servlet.
+//     *
+//     * @return a String containing servlet description
+//     */
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    }// </editor-fold>
+//
+//}
